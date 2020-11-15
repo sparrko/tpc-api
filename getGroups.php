54527811930741abb80ed772556eb973fe94700d
sparrko <55271713+sparrko@users.noreply.github.com>
2020-11-15 21:26:00 +0400

    Add files via upload

---
+++ getGroups.php
@@ -0,0 +1,35 @@
+<?php
+
+    // Vars
+    $url = 'http://www.tpcol.ru/asu/timetablestud.php?f=1';
+    
+    // Include
+    include 'system/s_getDom.php';
+
+    // Class
+    class Group {
+        public $name;           // название группы
+        public $value;          // ее код
+    }
+
+    // Init
+    $doc = s_getDom($url);
+    $xpath = new DOMXpath($doc);
+    
+    // Load and build data
+    $groups = array();
+    foreach ($xpath->query("//select[@name='group']/option/text()") as $name) {
+        $group = new Group();
+        $group->name = trim($name->nodeValue, " \t\n\r\0\x0B\xC2\xA0");
+        array_push($groups, $group);
+    }
+    $i = 0;
+    foreach ($xpath->query("//select[@name='group']/option/@value") as $value) {
+        $groups[$i]->value = trim($value->nodeValue, " \t\n\r\0\x0B\xC2\xA0");
+        $i++;
+    }
+
+    // Output
+    echo json_encode($groups);
+    
+?>
---
+++ getTimetable.php
@@ -0,0 +1,48 @@
+<?php
+
+    // Include
+    include 'system/s_getTable.php';
+    include 'system/s_getDayOfWeek.php';
+
+    // Vars
+    $url = 'http://www.tpcol.ru/asu/timetablestud.php?f=1';
+    $group = $_GET["group"];
+    $wday = s_getDayOfWeek();
+
+    // Classes
+    class Day {
+        public $tt_red;
+        public $tt_green;
+        public $tt_changes;
+
+        function init($day){
+            global $url, $group, $wday;
+            
+            $data = array('group' => $group, 'day' => $day, 'week' => "0");
+            $doc = s_getDom($url, $data);
+            $xpath = new DOMXpath($doc);
+            $this->tt_red = s_getTable(TABLE_SELECT, $doc);
+            
+            $data['week'] = "1";
+            $doc = s_getDom($url, $data);
+            $xpath = new DOMXpath($doc);
+            $this->tt_green = s_getTable(TABLE_SELECT, $doc);
+
+            if ($wday == $day) {
+                $this->tt_changes = s_getTable(TABLE_TODAY, $doc);
+            }
+            if ($wday + 1 == $day) {
+                $this->tt_changes = s_getTable(TABLE_TOMORROW, $doc);
+            }
+       }
+    }
+
+    // Act and output
+    $days = array();
+    for ($i = 1; $i < 6; $i++) {
+        $day = new Day();
+        $day->init($i);
+        array_push($days, $day);
+    }   
+    echo json_encode($days);
+?>
